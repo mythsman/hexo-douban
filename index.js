53bf4e7294b2213db73d4f4e58d33215fc071690
mythsman <mythsman@foxmail.com>
2017-06-16 12:48:47 +0800

    almost done

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 'use strict';
-var assign = require('object-assign');
 
-hexo.config.douban = assign({
-	per_page: typeof hexo.config.per_page === "undefined" ? 10 : hexo.config.per_page
-}, hexo.config.douban);
-
-hexo.extend.generator.register('books', require('./lib/generate-books'));
-//hexo.extend.generator.register('movies', require('./lib/generate-movies'));
+hexo.extend.generator.register('books', require('./lib/books-generator'));
+hexo.extend.generator.register('movies', require('./lib/movies-generator'));
