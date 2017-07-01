a2ef49cba89839d37d5a2528d607316d0f3d5373
mythsman <mythsman@foxmail.com>
2017-07-01 12:37:14 +0800

    init test-cov

--- test/index.js
+++ test/index.js
@@ -1,16 +1,9 @@
 'use strict';
 
-var should = require('chai').should(); // eslint-disable-line
-var Promise = require('bluebird');
 var Hexo = require('hexo');
 
 describe('Hexo douban', function() {
   var hexo = new Hexo(__dirname, {silent: true});
-
-  function locals() {
-    hexo.locals.invalidate();
-    return hexo.locals.toObject();
-  }
 
   before(function() {
     return hexo.init();
