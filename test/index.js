2fac47c5210c00ee20435ef2a3c0e7fdadf8d9f6
mythsman <mythsman@foxmail.com>
2018-01-10 22:24:08 +0800

    add games feature

--- test/index.js
+++ test/index.js
@@ -7,6 +7,7 @@ describe('Hexo douban', function() {
   var hexo = new Hexo(__dirname, {silent: true});
   var booksGenerator = Promise.method(require('../lib/books-generator').bind(hexo));
   var moviesGenerator = Promise.method(require('../lib/movies-generator').bind(hexo));
+  var gamesGenerator = Promise.method(require('../lib/games-generator').bind(hexo));
 
   function locals() {
     hexo.locals.invalidate();
@@ -20,6 +21,7 @@ describe('Hexo douban', function() {
   it('all disabled', function() {
     booksGenerator(locals());
     moviesGenerator(locals());
+    gamesGenerator(locals());
   });
 
   it('books enabled', function() {
@@ -44,6 +46,18 @@ describe('Hexo douban', function() {
       }
     };
     moviesGenerator(locals());
+  });
+
+  it('games enabled', function() {
+    this.timeout(150000);
+    hexo.config.douban = {
+      user: 'mythsman',
+      game: {
+        title: 'This is my game title',
+        quote: 'This is my game quote'
+      }
+    };
+    gamesGenerator(locals());
   });
 
 });
