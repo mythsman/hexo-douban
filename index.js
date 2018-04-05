f96adfcc042bbf4596633b8b2d8d5b36e9e94b6f
mythsman <mythsman@foxmail.com>
2018-04-05 16:55:27 +0800

    add lazyload feature

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 /* global hexo */
 'use strict';
+var fs = require('hexo-fs');
 
 hexo.extend.generator.register('books', require('./lib/books-generator'));
 hexo.extend.generator.register('movies', require('./lib/movies-generator'));
 hexo.extend.generator.register('games', require('./lib/games-generator'));
+
+hexo.extend.generator.register('douban-gif', function(locals) {
+  return {
+    path: '/assets/douban-loading.gif',
+    data: function() {
+      return fs.createReadStream(__dirname + '/lib/templates/douban-loading.gif');
+    }
+  };
+});
