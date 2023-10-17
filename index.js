7b7f378e0e7b4b27113be96842b247d82ed2c5b1
mythsman <mythsman@foxmail.com>
2023-10-17 20:41:08 +0800

    refactor code

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const fs = require('hexo-fs');
 const path = require('path');
 const log = require('./lib/util').log
 
-let init_from_console = false
-
 const supported_types = ['book', 'movie', 'game', 'song']
 
 // Register `hexo g` and `hexo s`
 supported_types.forEach(supported_type => {
     hexo.extend.generator.register(`${supported_type}s`, function (locals) {
-        if (init_from_console) {
-            return
-        }
         if (!this.config.douban || !this.config.douban[supported_type] || !this.config.douban.builtin) {
             return;
         }
         if (!this.config.douban[supported_type].path) {
             this.config.douban[supported_type].path = `${supported_type}s/index.html`
         }
+
+        if (this.config.douban[type].path.startsWith("/")) {
+            this.config.douban[type].path = this.config.douban[type].path.substr(1)
+        }
+
         locals.douban_type = supported_type
         return require(`./lib/generator`).call(this, locals);
     });
@@ -27,16 +27,15 @@ supported_types.forEach(supported_type => {
 
 const options = {
     options: [
-        {name: '-b, --books', desc: 'Generate douban books only'},
-        {name: '-m, --movies', desc: 'Generate douban movies only'},
-        {name: '-g, --games', desc: 'Generate douban games only'},
-        {name: '-s, --songs', desc: 'Generate douban songs only'}
+        { name: '-b, --books', desc: 'Generate douban books only' },
+        { name: '-m, --movies', desc: 'Generate douban movies only' },
+        { name: '-g, --games', desc: 'Generate douban games only' },
+        { name: '-s, --songs', desc: 'Generate douban songs only' }
     ]
 }
 
 // Register `hexo douban`
 hexo.extend.console.register('douban', 'Generate pages from douban', options, function (args) {
-    init_from_console = true
 
     if (!this.config.douban) {
         log.info("No douban config specified")
@@ -67,41 +66,30 @@ hexo.extend.console.register('douban', 'Generate pages from douban', options, fu
         })
     }
 
-    // Config preprocess
-    enabled_types.forEach(type => {
-        if (!this.config.douban[type].path) {
-            this.config.douban[type].path = `${type}s/index.html`
-        }
-    })
-
     if (enabled_types.length === 0) {
         log.info("No douban type specified")
         return
     }
 
-    const self = this;
-
-    //Register route
-    enabled_types.forEach(enabled_type => {
-        let page_path = self.config.douban[enabled_type].path || enabled_type + "/index.html"
-        if (page_path.startsWith("/")) {
-            page_path = page_path.substr(1)
+    // Prepare path
+    enabled_types.forEach(type => {
+        if (!this.config.douban[type].path) {
+            this.config.douban[type].path = `${type}s/index.html`
         }
-        self.config.douban[enabled_type].path = page_path
 
-        hexo.extend.generator.register(enabled_type, function (locals) {
-            locals.douban_type = enabled_type
-            return require(`./lib/generator`).call(this, locals)
-        });
+        if (this.config.douban[type].path.startsWith("/")) {
+            this.config.douban[type].path = this.config.douban[type].path.substr(1)
+        }
     })
 
+    const self = this;
 
     //Generate files
     self.load().then(function () {
         enabled_types.forEach(enabled_type => {
             const publicDir = self.public_dir;
             const id = self.config.douban[enabled_type].path
-            fs.mkdirSync(path.join(publicDir, id.replace("index.html", "")), {recursive: true})
+            fs.mkdirSync(path.join(publicDir, id.replace("index.html", "")), { recursive: true })
 
             let stream = self.route.get(id)
             if (stream) {
